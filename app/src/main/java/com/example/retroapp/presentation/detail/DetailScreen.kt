abe43997866312de8cb656ec17a5bb9f075bf14a
Merve OKTAY <merve.oktay@hotmail.com>
2023-07-18 17:19:37 +0300

    RegisterScreen note page added

---
+++ app/src/main/java/com/example/retroapp/presentation/detail/DetailScreen.kt
@@ -0,0 +1,18 @@
+package com.example.retroapp.presentation.detail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DetailScreen(){
+    Column(
+        modifier = Modifier
+            .background(color = Color.White)
+            .padding(20.dp, 60.dp)
+    ){}
+}
