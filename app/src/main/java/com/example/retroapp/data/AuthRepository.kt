db23805fd1c469de520eb465420ebb725c4be32d
orhanucr <ucar.orh@gmail.com>
2023-07-20 13:00:31 +0300

    username registration in firestore

--- app/src/main/java/com/example/retroapp/data/AuthRepository.kt
+++ app/src/main/java/com/example/retroapp/data/AuthRepository.kt
@@ -6,5 +6,6 @@ interface AuthRepository {
     val currentUser: FirebaseUser?
     suspend fun login(email: String, password: String): Resource<FirebaseUser>
     suspend fun signup(name: String, email: String, password: String): Resource<FirebaseUser>
+    suspend fun saveUsernameToDatabase(username: String, email: String): Resource<Boolean>
     fun logout()
 }
