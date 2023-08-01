d6a4a4c2c21898c86b219dfa5962035862bae722
orhanucr <ucar.orh@gmail.com>
2023-08-01 16:57:57 +0300

    A logo has been added to the header section and the necessary color adjustments have been made.

--- app/src/main/java/com/example/retroapp/presentation/auth/Login.kt
+++ app/src/main/java/com/example/retroapp/presentation/auth/Login.kt
@@ -3,6 +3,7 @@ package com.example.retroapp.presentation.auth
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -72,7 +74,7 @@ fun LoginScreen(viewModel: AuthViewModel?, navController: NavController) {
             label = {
                 Text(text = stringResource(id = R.string.email))
             },
-            modifier = Modifier.constrainAs(refEmail) {
+            modifier = Modifier.constrainAs(refEmail){
                 top.linkTo(refHeader.bottom, spacing.extraLarge)
                 start.linkTo(parent.start, spacing.large)
                 end.linkTo(parent.end, spacing.large)
