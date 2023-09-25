392b3b5f48514a6d7a3b57cd999c9a0d0d14f502
orhanucr <ucar.orh@gmail.com>
2023-09-25 16:57:28 +0300

    first commit AuthRepositoryImplTest

---
+++ app/src/test/java/com/example/retroapp/data/AuthRepositoryImplTest.kt
@@ -0,0 +1,32 @@
+package com.example.retroapp.data
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class AuthRepositoryImplTest {
+
+    @Test
+    fun getCurrentUser() {
+    }
+
+    @Test
+    fun login() {
+    }
+
+    @Test
+    fun signup() {
+    }
+
+    @Test
+    fun saveUsernameToDatabase() {
+    }
+
+    @Test
+    fun logout() {
+    }
+
+    @Test
+    fun isUserLoggedIn() {
+    }
+}
